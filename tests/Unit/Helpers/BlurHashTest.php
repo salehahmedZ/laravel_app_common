cedc1c58ca934ab33543f604f4ffcb026353d46c
Saleh Alzahrani <saleh_ahmed@outlook.com>
2022-12-14 19:08:06 -0600

    blurhash test

---
+++ tests/Unit/Helpers/BlurHashTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Saleh\LaravelAppCommon\Helpers\BlurHash;
+use function Pest\testDirectory;
+
+test('get blurhash', function () {
+    //null
+    $blurHash= BlurHash::make(null);
+    expect($blurHash)->toBe(null);
+
+
+    $path = testDirectory('Unit/data/1.jpg');
+
+    //local path jpg
+    $blurHash= BlurHash::make($path);
+    expect($blurHash)->toBe('LMK,T~-=px}xtk-;IUX,%ixKS]+#');
+
+    //url png
+    $path = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
+    $blurHash= BlurHash::make($path);
+    expect($blurHash)->toBe('LnG9wSnXENOuY%aMi]e.49nNwFvz');
+
+
+    //not an image url
+    $path = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp';
+    $blurHash= BlurHash::make($path);
+    expect($blurHash)->toBe(null);
+
+});
