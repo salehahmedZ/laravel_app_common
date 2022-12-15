5609b51275822e3a0baa73ad72be7c3da2b1b4ce
Saleh Alzahrani <saleh_ahmed@outlook.com>
2022-12-14 19:56:10 -0600

    Update DistanceTest.php

--- tests/Unit/Helpers/DistanceTest.php
+++ tests/Unit/Helpers/DistanceTest.php
@@ -7,7 +7,7 @@ test('get distance', function () {
     $result = Distance::get(24.713551, 46.675296, 24.713551, 46.675296);
     expect($result)->toBe(0);
 
-    //0
+    //149
     $result = Distance::get(24.713551, 46.675296, 25.713551, 47.675296);
     expect($result)->toBe(149.94797088295556);
 });
